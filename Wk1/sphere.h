f549744d13bcb1ccd00a5a5569e1fdf6337bf36e
delekgm <delekgmiller@gmail.com>
2024-04-08 12:18:43 -0700

    20240408a

--- Wk1/sphere.h
+++ Wk1/sphere.h
@@ -42,3 +42,16 @@ bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) cons
     return true;
 }
 #endif
+
+// double hit_sphere(const point3 &center, double radius, const ray &r){
+//     vec3 oc = r.origin() - center;
+//     auto a = r.direction().length_squared();
+//     auto half_b = dot(oc, r.direction());
+//     auto c = oc.length_squared() - radius * radius;
+//     auto discriminant = half_b * half_b - a * c;
+//     if (discriminant < 0){
+//         return -1.0;
+//     } else {
+//         return (-half_b - sqrt(discriminant)) / (a);
+//     }
+// }
